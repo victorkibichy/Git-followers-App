540875f05bdc1a1ac01dc326f93210b29b7c69b8
victor <victorkibichy>
2024-05-14 12:40:41 +0300

    first commit

---
+++ Git Followers/FavoritesListVC.swift
@@ -0,0 +1,30 @@
+//
+//  FavoritesListVC.swift
+//  Git Followers
+//
+//  Created by  Bouncy Baby on 5/14/24.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Git Followers/SearchVC.swift
@@ -0,0 +1,29 @@
+//
+//  SearchVC.swift
+//  Git Followers
+//
+//  Created by  Bouncy Baby on 5/14/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+
+      }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

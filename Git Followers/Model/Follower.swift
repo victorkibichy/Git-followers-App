7fbe69f6fbd3a6cdba989f597c3ae20267bd1a21
victor <victorkibichy>
2024-05-21 11:57:12 +0300

    model organization

---
+++ Git Followers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  Git Followers
+//
+//  Created by  Bouncy Baby on 5/21/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ Git Followers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  Git Followers
+//
+//  Created by  Bouncy Baby on 5/21/24.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}

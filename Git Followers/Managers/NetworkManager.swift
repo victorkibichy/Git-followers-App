4b5d789008513a09f58d834111d6af588500dc2c
victor <victorkibichy>
2024-05-23 11:46:47 +0300

    URL fixes

---
+++ Git Followers/Managers/NetworkManager.swift
@@ -0,0 +1,53 @@
+//
+//  NetworkManager.swift
+//  Git Followers
+//
+//  Created by  Bouncy Baby on 5/21/24.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    let baseURL = "https://api.github.com/users/"
+    
+    private init() { }
+    
+    func getFollwers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void) {
+        let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
+        
+        guard let url = URL(string: endpoint) else {
+            completed(nil, "This username created an invalid request. Please try again")
+            return
+        }
+        
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completed(nil, "Unable to complete your request. Please check your internet connection")
+            }
+            
+            guard let response = response as? HTTPURLResponse,response.statusCode == 200 else {
+                completed(nil, "Invalid response from the server. Please try again.")
+                
+                return
+            }
+            guard let data = data else{
+                completed(nil, "The data received from server was invalid. Please Try again")
+                
+                return
+                
+            }
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let followers = try decoder.decode([Follower].self, from: data)
+                completed(followers, nil)
+            } catch {
+                completed(nil, "The data received from the server was invalid. Please try again")
+            }
+        }
+        task.resume()
+    }
+}
--- Git Followers/Screens/FollowerListVC.swift
+++ Git Followers/Screens/FollowerListVC.swift
@@ -3,6 +3,10 @@
 //  Git Followers
 //
 //  Created by  Bouncy Baby on 5/15/24.
+
+
+
+
 //
 
 import UIKit
@@ -17,8 +21,22 @@ class FollowerListVC: UIViewController {
         navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
 
+        
+        NetworkManager.shared.getFollwers(for: username, page: 1) { (followers, errorMesssage) in
+            
+            guard let followers = followers else {
+                
+                self.presentGFAlertOnMainThread(title: "BAD STUFF HAPPENED", message: errorMesssage!, buttonTitle: "OK")
+                return
+            }
+            print("Followers.count = \(followers.count)")
+            print(followers)
+        }
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
 
 }

e7fc29329f798080404fa692471b85c71834255a
victor <victorkibichy>
2024-05-31 09:54:29 +0300

    cell config

--- Git Followers/Custom Views/Cells/FollowerCell.swift
+++ Git Followers/Custom Views/Cells/FollowerCell.swift
@@ -8,5 +8,40 @@
 import UIKit
 
 class FollowerCell: UICollectionViewCell {
-    static let reuseID = "FollowerCell" 
+    
+    static let reuseID = "FollowerCell"
+        
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        
+        let padding: CGFloat = 8
+        
+        
+        NSLayoutConstraint.activate([
+        
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor)
+            
+            
+        ])
+    }
+    
 }

32207441f9e61f066328eb6f6aa6fa3a0fae37db
victor <victorkibichy>
2024-06-05 08:10:41 +0300

    configurations of the follower cells

--- Git Followers/Custom Views/Cells/FollowerCell.swift
+++ Git Followers/Custom Views/Cells/FollowerCell.swift
@@ -23,6 +23,11 @@ class FollowerCell: UICollectionViewCell {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+        
+    }
+    
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
     }
     
     private func configure() {
@@ -38,8 +43,12 @@ class FollowerCell: UICollectionViewCell {
             avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
             avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
             avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
-            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor)
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
             
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
             
         ])
     }
--- Git Followers/Screens/FollowerListVC.swift
+++ Git Followers/Screens/FollowerListVC.swift
@@ -14,13 +14,42 @@ import UIKit
 class FollowerListVC: UIViewController {
     
     var username: String!
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
+        configureViewController()
+        getFollowers()
+      
+    
+        
+        func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            navigationController?.setNavigationBarHidden(false, animated: true)
+        }
+        
+ 
+    }
+    
+    func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
         
+        
+    }
+    
+    
+    
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func getFollowers() {
         
         NetworkManager.shared.getFollwers(for: username, page: 1) { result in
             
@@ -37,11 +66,5 @@ class FollowerListVC: UIViewController {
             
             
         }
-        
-        func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            navigationController?.setNavigationBarHidden(false, animated: true)
-        }
-        
     }
 }
